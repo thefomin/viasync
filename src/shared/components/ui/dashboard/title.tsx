9c0f261c8878f7442164621a262f7112799ea4b9
ravey <ravexion@gmail.com>
2025-04-09 22:13:11 +0700

    fix: update logic title UI

--- src/shared/components/ui/dashboard/title.tsx
+++ src/shared/components/ui/dashboard/title.tsx
@@ -2,15 +2,12 @@
 
 import { IDocument } from '@/shared/types/document/document.types'
 import TextareaAutosize from 'react-textarea-autosize'
-import { useCallback } from 'react'
 import { Popover, PopoverContent, PopoverTrigger, Skeleton } from '..'
-import { useUpdateMutation } from '@/shared/hooks/document'
-import { debounce } from 'lodash'
 import { documentsApiQuery } from '@/shared/api'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { IconPicker } from './document'
 import { FileIcon } from 'lucide-react'
-import { QueryClientService } from '@/shared/services/document'
+import { useDocumentUpdater } from '@/shared/hooks/document/useDocumentUpdater'
 
 interface TitleProps {
 	initialData: IDocument
@@ -18,10 +15,7 @@ interface TitleProps {
 }
 
 export const Title = ({ initialData, children }: TitleProps) => {
-	// const [isEditing, setIsEditing] = useState(false)
 	const queryClient = useQueryClient()
-	const queryClientService = new QueryClientService(queryClient)
-	const { update } = useUpdateMutation()
 
 	const { data: documents } = useQuery({
 		queryKey: [documentsApiQuery.baseKey],
@@ -34,73 +28,17 @@ export const Title = ({ initialData, children }: TitleProps) => {
 	const title = currentDocument?.title || initialData.title
 	const icon = currentDocument?.icon || initialData.icon
 
-	const disabledInput = () => {
-		// setIsEditing(false)
-		if (title !== initialData.title) {
-			initialData.title = title
-		}
-	}
-	/* eslint-disable-next-line react-hooks/exhaustive-deps */
-	const debouncedUpdate = useCallback(
-		debounce(
-			(newTitle: string) => {
-				update({
-					id: initialData.id,
-					data: {
-						title: newTitle || 'Untitled',
-						updatedAt: new Date().toISOString()
-					}
-				})
-			},
-			500,
-			{ leading: false, trailing: true }
-		),
-		[initialData.id]
-	)
-
+	const documentUpdater = useDocumentUpdater(initialData)
 	const onChange = (value: string) => {
-		queryClientService.updateAllFields(
-			initialData,
-			{
-				title: value,
-				updatedAt: new Date().toISOString()
-			},
-			{ updateFavorites: true }
-		)
-
-		debouncedUpdate(value)
+		documentUpdater.updateField('title', value)
 	}
 
-	const handleEmojiSelect = (icon: string) => {
-		queryClientService.updateAllFields(
-			initialData,
-			{
-				icon,
-				updatedAt: new Date().toISOString()
-			},
-			{ updateFavorites: true }
-		)
-
-		update({
-			id: initialData?.id,
-			data: { icon, updatedAt: new Date().toISOString() }
-		})
+	const handleEmojiSelect = (emoji: { native: string }) => {
+		documentUpdater.updateField('icon', emoji.native)
 	}
 
-	const onKeyDown = (event: React.KeyboardEvent) => {
-		if (event.key === 'Enter') {
-			disabledInput()
-		}
-	}
 	const deleteIcon = () => {
-		queryClientService.updateAllFields(initialData, {
-			icon: '',
-			updatedAt: new Date().toISOString()
-		})
-		update({
-			id: initialData?.id,
-			data: { icon: '', updatedAt: new Date().toISOString() }
-		})
+		documentUpdater.deleteField('icon')
 	}
 
 	return (
@@ -136,9 +74,7 @@ export const Title = ({ initialData, children }: TitleProps) => {
 					</IconPicker>
 
 					<TextareaAutosize
-						onBlur={disabledInput}
 						onChange={e => onChange(e.target.value)}
-						onKeyDown={onKeyDown}
 						value={title || ''}
 						placeholder='Untitled'
 						className='w-full resize-none break-words rounded-sm border border-primary/10 bg-primary/5 px-2 py-0.5 text-foreground outline-none'
