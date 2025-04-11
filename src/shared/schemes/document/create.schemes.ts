ee5d7d351e8dee0bdcae8900628651c6ed349d75
ravey <ravexion@gmail.com>
2025-04-11 13:16:04 +0700

    feat: added isBoard to create schema

--- src/shared/schemes/document/create.schemes.ts
+++ src/shared/schemes/document/create.schemes.ts
@@ -4,7 +4,8 @@ export const CreateSchema = z.object({
 	parentDocumentId: z.optional(z.string()),
 	columnId: z.optional(z.string()),
 	title: z.optional(z.string()),
-	description: z.optional(z.string())
+	description: z.optional(z.string()),
+	isBoard: z.optional(z.boolean())
 })
 
 export type TypeCreateSchema = z.infer<typeof CreateSchema>
