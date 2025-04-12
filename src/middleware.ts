84e5343d7d14a834766a418a08fc54bb353a369c
ravey <ravexion@gmail.com>
2025-04-12 12:47:22 +0700

    fix: matcher to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,5 +18,5 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ['/auth/:path*', '/dashboard/:path*', '/']
+	matcher: ['/((?!_next|api|auth|preview).*)']
 }
